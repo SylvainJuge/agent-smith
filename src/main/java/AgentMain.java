35e9d91848fbde23611a21469fd3081ad5bbccfd
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2024-06-12 22:15:26 +0200

    implement simple dump agent

--- src/main/java/AgentMain.java
+++ src/main/java/AgentMain.java
@@ -1,11 +1,80 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
 import java.lang.instrument.Instrumentation;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.ProtectionDomain;
+import java.util.Comparator;
 
 public class AgentMain {
 
     private static Instrumentation instrumentation;
 
     public static void premain(String args, Instrumentation inst) {
-        System.out.println("agent premain");
         instrumentation = inst;
+
+        final Path dumpFolder = getDumpFolder();
+
+        System.out.println("dump bytecode in " + dumpFolder);
+
+        inst.addTransformer(new ClassFileTransformer() {
+            @Override
+            public byte[] transform(ClassLoader cl, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
+                dumpClass(dumpFolder, cl, className, classfileBuffer);
+                return null; // do not modify any class for now
+            }
+        });
+    }
+
+    private static void dumpClass(Path folder, ClassLoader cl, String className, byte[] bytecode) {
+        String clClassName = cl.getClass().getName();
+        if (clClassName.endsWith("DelegatingClassLoader")) {
+            // ignore internal classloader
+            return;
+        }
+        Path clFolderPath = folder.resolve(normalizePath(cl.toString()));
+        try {
+            Files.createDirectories(clFolderPath);
+            Path filePath = clFolderPath.resolve(normalizePath(className) + ".class");
+            Files.write(filePath, bytecode);
+        } catch (IOException e) {
+            throw new IllegalStateException("unable to dump class file");
+        }
+    }
+
+    private static String normalizePath(String s){
+        return s.replaceAll("/", "_")
+                .replaceAll("\\$", "_")
+                .replaceAll("@", "_");
+    }
+
+    private static Path getDumpFolder() {
+        String dumpPathConfig = System.getProperty("smith.dump_path");
+        if (dumpPathConfig == null) {
+            Path tmp = Paths.get(System.getProperty("java.io.tmpdir"));
+            try {
+                return Files.createTempDirectory(tmp, "agent-smith");
+            } catch (IOException e) {
+                throw new IllegalStateException("unable to create temp folder");
+            }
+        }
+        Path dumpPath = Paths.get(dumpPathConfig);
+
+        // recursively delete
+        try {
+            Files.walk(dumpPath).sorted(Comparator.reverseOrder()).forEach(path -> {
+                try {
+                    Files.delete(path);
+                } catch (IOException e) {
+                    // silently ignored
+                }
+            });
+        } catch (IOException e) {
+            throw new IllegalStateException("unable to delete temp folder");
+        }
+        return dumpPath;
     }
 }
