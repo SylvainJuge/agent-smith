ff5fe1be06be2b237a2ee388a3e165b7089c38e6
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2024-07-10 13:52:22 +0200

    add helloworld

---
+++ examples/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+
+    public void damageHello(int dmg, Object src){
+        dmg = 0;
+        System.out.println("damage!");
+    }
+}
