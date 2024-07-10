d79fe02469f04efb7b3a5dc42c0caa0a8e0977ae
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2024-07-10 16:06:28 +0200

    remove linux part

--- examples/shattered-pixel-dungeon/run.sh
+++ examples/shattered-pixel-dungeon/run.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
 
-folder="$(dirname $0)"
+folder="$(dirname "${0}")"
 
 scenario="${1:-0}"
+
+# extra JVM option needed for macos only
+macos_option=''
+java -X 2>&1|grep -q 'startOnFirstThread'
+if [ $? == 0 ]; then
+  macos_option='-XstartOnFirstThread'
+fi
 
 case ${scenario} in
   0)
     echo "run game"
     java \
+      ${macos_option} \
       -jar ${folder}/shattered-pixel-dungeon.jar
     ;;
   1)
     echo "run game and dump classes in ${folder}/class_dump"
     java \
-      -javaagent:${folder}/../../../target/smith-agent-1.0-SNAPSHOT.jar \
+      ${macos_option} \
+      -javaagent:${folder}/../../target/smith-agent-1.0-SNAPSHOT.jar \
       -Dsmith.dump_path=${folder}/class_dump \
       -jar ${folder}/shattered-pixel-dungeon.jar
     ;;
   2)
     echo "run game with cheat"
     java \
-      -javaagent:${folder}/../../../target/smith-agent-1.0-SNAPSHOT.jar \
+      ${macos_option} \
+      -javaagent:${folder}/../../target/smith-agent-1.0-SNAPSHOT.jar \
       -Dsmith.cheat=true \
       -jar ${folder}/shattered-pixel-dungeon.jar
     ;;
